4add3fb2a5be0b32eb812158e561dec7ad62ae4f
SumitPandey08 <sp.sumitpandey06@gmail.com>
2024-11-10 15:40:12 +0530

    db connect

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constents";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) ;
+       console.log(`\n MONGODB Connected DB Host : ${connectionInstance.connection.host}`);
+       
+    } catch (error) {
+        console.log("ERROR AT CONNECTION MONGODB :" , error);
+        process.exit(1) ;
+        
+    }
+}
+
+export default connectDB ;
